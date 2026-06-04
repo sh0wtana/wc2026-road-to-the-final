a3ac19bc1a4c144567592f26af25bf85e536629c
shtan14 <wrzn.inw.snun@gmail.com>
2026-06-04 22:07:34 +0900

    feat: add GroupSection with all 12 groups

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,10 @@
 <script>
   import Header from './components/Header.svelte'
-  import GroupCard from './components/GroupCard.svelte'
-  import { state, resetState } from './store.svelte.js'
+  import GroupSection from './components/GroupSection.svelte'
+  import { resetState } from './store.svelte.js'
 </script>
 
 <div class="min-h-screen bg-gray-950 text-white">
   <Header onReset={resetState} />
-  <div class="p-4">
-    <GroupCard
-      group="A"
-      teams={state.groups.A}
-      onReorder={(items) => { state.groups.A = items }}
-    />
-  </div>
+  <GroupSection />
 </div>
---
+++ src/components/GroupSection.svelte
@@ -0,0 +1,18 @@
+<script>
+  import GroupCard from './GroupCard.svelte'
+  import { state } from '../store.svelte.js'
+
+  const GROUPS = ['A','B','C','D','E','F','G','H','I','J','K','L']
+</script>
+
+<section class="px-4 py-4 border-b border-gray-800">
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3">
+    {#each GROUPS as g}
+      <GroupCard
+        group={g}
+        teams={state.groups[g]}
+        onReorder={(items) => { state.groups[g] = items }}
+      />
+    {/each}
+  </div>
+</section>
